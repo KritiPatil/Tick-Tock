13f222b31ee619ceb3be591d2f03b51dc51ffe9c
KritiPatil <66488628+KritiPatil@users.noreply.github.com>
2020-10-21 15:56:03 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -15,39 +15,39 @@ function draw() {
 
   scAngle = map(sc, 0, 60, 0, 360);
   mnAngle = map(mn, 0, 60, 0, 360);
-  hrAngle = map(hr, 0, 12, 0, 360);
+  hrAngle = map(hr%12, 0, 12, 0, 360);
 
-  console.log(hr);
-  console.log(mn);
-  console.log(sc);
+  //console.log(hr);
+  //console.log(mn);
+  //console.log(sc);
 
-  translate(width, height);
+  translate(300, 300);
   
   //seconds
   push ();
   rotate(scAngle);
-  translate(p5.Vector.fromAngle(millis() / 60000, 40));
+  //translate(p5.Vector.fromAngle(millis() / 60000, 40));
   stroke(255, 20, 147);
   strokeWeight(7);
-  line(300, 300, 50, 200);
+  line(0, 0, 100, 150);
   pop(); 
   
   //minutes
   push ();
   rotate(mnAngle);
-  translate(p5.Vector.fromAngle(millis() / 60000, 40));
+  //translate(p5.Vector.fromAngle(millis() / 60000, 40));
   stroke(0, 206, 209);
   strokeWeight(7);
-  line(300, 300, 50, 200);
+  line(0, 0, 100, 125);
   pop(); 
 
   //hours
   push ();
   rotate(hrAngle);
-  translate(p5.Vector.fromAngle(millis() / 60000, 40));
+  //translate(p5.Vector.fromAngle(millis() / 60000, 40));
   stroke(148, 0, 211);
   strokeWeight(7);
-  line(300, 300, 200, 200);
+  line(0, 0, 100, 100);
   pop(); 
 
   drawSprites();
